9be9e47bcd1f873a6a2f5f92e8b033932a02c6ca
Domi <dominika@nozbe.com>
2020-07-23 11:22:40 +0200

    [App.js] Add container class

--- src/App.js
+++ src/App.js
@@ -8,26 +8,28 @@ import ExtraInfo from "./ExtraInfo";
 function App() {
   return (
     <div className="App">
-      <div className="WeatherApp">
-        <SearchBar />
-        <Today />
-        <ExtraInfo />
-        <div className="row weather-forecast" id="forecast">
-          <Forecast weekDay="MON" tempMax="21" tempMin="16" />
-          <Forecast weekDay="TUE" tempMax="20" tempMin="16" />
-          <Forecast weekDay="WED" tempMax="22" tempMin="18" />
-          <Forecast weekDay="THU" tempMax="23" tempMin="18" />
-          <Forecast weekDay="FRI" tempMax="24" tempMin="19" />
+      <div className="container">
+        <div className="WeatherApp">
+          <SearchBar />
+          <Today />
+          <ExtraInfo />
+          <div className="row weather-forecast" id="forecast">
+            <Forecast weekDay="MON" tempMax="21" tempMin="16" />
+            <Forecast weekDay="TUE" tempMax="20" tempMin="16" />
+            <Forecast weekDay="WED" tempMax="22" tempMin="18" />
+            <Forecast weekDay="THU" tempMax="23" tempMin="18" />
+            <Forecast weekDay="FRI" tempMax="24" tempMin="19" />
+          </div>
         </div>
-      </div>
-      <div className="coded-by">
-        <small>
-          <a href="https://github.com/dmusiol/weather-react">
-            {" "}
-            Open-source code
-          </a>{" "}
-          by Dominika Musiol 👩🏻‍💻
-        </small>
+        <div className="coded-by">
+          <small>
+            <a href="https://github.com/dmusiol/weather-react">
+              {" "}
+              Open-source code
+            </a>{" "}
+            by Dominika Musiol 👩🏻‍💻
+          </small>
+        </div>
       </div>
     </div>
   );
